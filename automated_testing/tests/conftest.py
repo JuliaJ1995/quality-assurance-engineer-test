73a09960a7c2c7b1bea1ef0d4e8d4bcb861be194
Julia Jaroszuk <juliajaroszuk@gmail.com>
2021-12-12 11:34:50 +0100

    pdf with insights was added and description in conftest was updated

--- automated_testing/tests/conftest.py
+++ automated_testing/tests/conftest.py
@@ -7,10 +7,11 @@ from automated_testing.run_all_tests import URL
 @pytest.fixture(autouse=True, scope='class')
 def driver():
     """
-    This function will be run with every test.
-    At the beginning of each test, it will connect to Firefox browser and open page given in the URL variable.
-    After tests is executed, with positive or negative result,
-     this function will close and end the current browser session.
+    This function will be run for every test class.
+    At the beginning of each test set (tests defined in one class),
+    it will connect to the defined browser and open page given in the URL variable.
+    After all tests from the class are executed, with positive or negative results,
+    this function will close and end the current browser session.
     """
     browser = get_browser()
     browser.get(URL)
